5147a4911a1fdedc07298235d0ba89d1fe370ba5
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-02-04 18:20:43 -0800

    6

--- Code/listogram.py
+++ Code/listogram.py
@@ -21,24 +21,52 @@ class Listogram(list):
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
+        if word in self:
+            index = self.index_of(word)
+            current_length = self[index][1] + count
+            self.pop(index)
+            self.append((word, current_length))
+        else:
+            self.append((word, count))
+            self.types += 1
+            self.tokens += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        index = self.index_of(word.lower())
+        if index is not None:
+            return self[index][1]
+        else:
+            return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
         # TODO: Check if word is in this histogram
+        for sub_list in self:
+            if sub_list[0] == word:
+                return True
+        return False
 
     def index_of(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
         # TODO: Implement linear search to find index of entry with target word
+        for index, list in enumerate(self):
+            if target.lower() in list:
+                return index
+        return None
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
         # TODO: Randomly choose a word based on its frequency in this histogram
+        target_number = random.uniform(0, self.tokens)
+        acc = 0
+        for word, length in self:
+            acc += length
+            if acc >= target_number:
+                return word
 
 
 def print_histogram(word_list):
