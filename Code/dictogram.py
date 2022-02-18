fe139dd404addca3bc2f082b575e5096ea2d666b
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-02-17 17:22:52 -0800

    tennis

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -17,6 +17,7 @@ class Dictogram(dict):
         if word_list is not None:
             for word in word_list:
                 self.add_count(word)
+                self.frequency(word)
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
